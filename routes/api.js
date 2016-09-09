224b496b4b0c9df860af1480e322a33081b6d287
Stephanie Ogburn <stephanie.ogburn@gmail.com>
2016-09-08 22:47:47 -0600

    updated response from successful post

--- routes/api.js
+++ routes/api.js
@@ -4,46 +4,43 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-router.get('/home', function(req, res, next){
-  res.send("<h1>You Made It!!</h1>");
-})
-
 
 router.post('/city-wise', function(req,res,next){
   var wiseup = {
     category: req.body.category,
     issue: req.body.issue,
-    photo_url: req.body.url,
+    photo_url: req.body.photo_url,
     user_email: req.body.user_email,
     lat: req.body.lat,
     long: req.body.long
   }
   knex('locations').where({city_name: req.body.city, state_abbr: req.body.state})
   .then(function(data){
-    console.log("data when city doesn't exist:", data);
     if(data.length === 0){
+      console.log("data when city doesn't exist:", data);
       knex('locations').insert({city_name: req.body.city, state_abbr: req.body.state})
       .returning('id')
       .then(function(id){
       wiseup.city_id = id;
-      knex('wiseups').insert(wiseup)
+      knex('wiseups').insert(wiseup).returning("*")
+      })
       .then(function(data){
-      console.log(data);
-        })
+        res.json(data);
       })
     }
     else {
       console.log('the city exists and here is the data:', data[0]);
       wiseup.city_id = data[0].id;
       console.log('wiseup:', wiseup);
-      knex('wiseups').insert(wiseup)
+      knex('wiseups').insert(wiseup).returning("*")
       .then(function(data){
-        console.log(data);
+        console.log('data', data);
+        res.json(data);
       })
     }
   })
   .catch(function(err){
-    console.log(err);
+    res.json(err);
   })
 });
 
